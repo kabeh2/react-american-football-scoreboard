148321495214a0cc54bb5c185593c06fc218f13d
kabeh2 <mattcnk@hotmail.com>
2019-10-04 22:47:24 -0400

    Updates

---
+++ precache-manifest.2e08571d7d4a314d17d2c6aadec0a8bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dcc3fda2d5bf6e5c71bab85d6ce657b",
+    "url": "/react-american-football-scoreboard/index.html"
+  },
+  {
+    "revision": "287b4f6edf16c1c8f94b",
+    "url": "/react-american-football-scoreboard/static/css/main.7d1921f0.chunk.css"
+  },
+  {
+    "revision": "1ed47eb8a12884a418eb",
+    "url": "/react-american-football-scoreboard/static/js/2.3db1a4b2.chunk.js"
+  },
+  {
+    "revision": "287b4f6edf16c1c8f94b",
+    "url": "/react-american-football-scoreboard/static/js/main.b24abe55.chunk.js"
+  },
+  {
+    "revision": "8df6c3978f6d0d1e7c2a",
+    "url": "/react-american-football-scoreboard/static/js/runtime~main.e9fbb0de.js"
+  },
+  {
+    "revision": "1abd8c1a58598974d8415573764ba76a",
+    "url": "/react-american-football-scoreboard/static/media/Ozone.1abd8c1a.ttf"
+  }
+]);
